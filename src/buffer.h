a680324e5e0d1f33b98b6531aa8cf9d5f5ff7f3c
Sebastian Mendez <sebas.mi833@gmail.com>
2025-06-01 21:32:13 -0500

    Update buffer.h

--- src/buffer.h
+++ src/buffer.h
@@ -12,8 +12,10 @@ typedef struct {
     char operacion;
     char nombreLibro[MAX_LIBRO];
     int isbn;
+    int ejemplar; // ✅ NUEVO
     char pipeRespuesta[MAX_PIPE];
 } Mensaje;
+
 
 typedef struct {
     Mensaje datos[TAM_BUFFER];
