fd67ed36c612f0b65b74685a5072a4caf75a926f
Sebastian Mendez <sebas.mi833@gmail.com>
2025-06-02 10:42:19 -0500

    Update receptor.c

--- src/receptor.c
+++ src/receptor.c
@@ -17,8 +17,6 @@ BufferCircular bufferGlobal;
 int sistemaActivo = 1;
 char archivoSalida[100] = "";
 int verboseFlag = 0;
-RegistroOperacion historial[MAX_REGISTROS];
-int total_registros = 0;
 
 
 void *hiloAuxiliar1(void *arg);
